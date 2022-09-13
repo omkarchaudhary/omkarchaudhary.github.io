854e7f55543f8bd7a27e565f993eda1889bce103
omkar chaudhary <omkar.chaudhary@miu.edu>
2022-09-12 23:06:53 -0500

    assignment for lab5

--- js/login.js
+++ js/login.js
@@ -19,5 +19,6 @@ function validateForm(){
     console.log("Password "+passwordText.value);
     console.log("Regex pattern match "+regexPattern.test(passwordText.value))
 }
-
+//(\+1)?(\s)?(\d{3}\s+\d{3}\s+\d{4})|(\d{3}\d{3}\d{4}) - US number
+//^(\+1)?(\s)?(\d{3}\s\d{3}\s\d{4})|(\d{3}\d{3}\d{4})$
 }

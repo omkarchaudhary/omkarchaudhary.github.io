41af251d2df22873f33b20bf3df947b301fe04d6
omkar chaudhary <omkar.chaudhary@miu.edu>
2022-09-12 19:35:13 -0500

    assignment for lab5-part2

---
+++ js/login.js
@@ -0,0 +1,23 @@
+window.onload = function(){
+    "use strict";
+const submitBtn = document.getElementById("submitBtn");
+const emailText = document.getElementById("email");
+const passwordText = document.getElementById("password");
+const checkbox = document.getElementById("checkbox");
+const regexPattern = new RegExp("[a-zA-Z0-9]{10,}");
+
+submitBtn.onclick = validateForm;
+function validateForm(){
+    console.log("Password "+passwordText.value);
+    console.log("Regex pattern match "+regexPattern.test(passwordText.value))
+    if(!regexPattern.test(passwordText.value)){
+        alert("Password field is at least 10 characters and contain at least one number and one uppercase and lowercase letter")
+    }
+    else{
+        alert("Form validated successfully");
+    }
+    console.log("Password "+passwordText.value);
+    console.log("Regex pattern match "+regexPattern.test(passwordText.value))
+}
+
+}

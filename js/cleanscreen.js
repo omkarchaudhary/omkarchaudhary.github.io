ae660cfdb342ff878e6fc3657a26022912abca4b
omkar chaudhary <omkar.chaudhary@miu.edu>
2022-09-15 08:07:00 -0500

    lab8 assignment

---
+++ js/cleanscreen.js
@@ -0,0 +1,43 @@
+window.onload = function(){
+    let intervalId = null;
+    let growthRate =250;
+    const circleDiv = document.getElementById("circle");
+    const circleWidth = document.getElementById("width");
+    const circleGrowthAmount = document.getElementById("amount");
+    const circleGrowthRate = document.getElementById("rate");
+    const circleNumber = document.getElementById("circle-number");
+    const startBtn = document.getElementById("start-button");
+    circleDiv.onclick = stop;
+    startBtn.onclick = growCircleByInput;
+    
+    function animation(rate){
+        if(intervalId == null){
+            intervalId = setInterval(function(){
+                let height = circleDiv.clientHeight;
+                let width = circleDiv.clientWidth;
+                circleDiv.style.height = height+10+"px";
+                circleDiv.style.width = width+10+"px";
+                circleDiv.style.borderRadius = height/2 +"px";
+                 
+            },rate);
+        }
+    }
+    animation(growthRate);
+
+    function growCircleByInput(){
+        circleDiv.style.height = circleGrowthAmount.value+"px";
+        circleDiv.style.width = circleWidth.value+"px";
+        circleDiv.style.borderRadius = circleWidth.value/2 +"px";
+        animation(circleGrowthRate.value);
+    }
+    function stop(){
+        clearInterval(intervalId);
+        resetCircle();
+        intervalId= null;
+    }
+    function resetCircle(){
+        circleDiv.style.height = "0px";
+        circleDiv.style.width = "0px";
+        circleDiv.style.borderRadius = "0px";
+    }
+}
